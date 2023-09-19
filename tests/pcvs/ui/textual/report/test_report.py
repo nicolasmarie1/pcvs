979a2d814f0896d9300772bef63f7d543e1ee3d9
Julien Adam <adamj@paratools.com>
2023-09-19 11:37:33 +0200

    COVERAGE: add UI tests

---
+++ tests/pcvs/ui/textual/report/test_report.py
@@ -0,0 +1,17 @@
+import os
+from unittest.mock import patch
+
+import pcvs
+from pcvs.ui.textual import report as tested
+
+from .conftest import click_call
+from .conftest import isolated_fs
+
+
+def test_loaded_tui():
+    with isolated_fs():
+        res = click_call('profile', 'create', 'local.default')
+        res = click_call('run')
+        res = click_call('--tui', 'report')
+        print(res.stderr)
+        assert(res.exit_code == 0)
