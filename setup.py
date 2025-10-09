75916ae31318768077e472a8f4c22c3d3662b0ce
Nicolas Marie <nicolas.marie@uvsq.fr>
2025-10-09 10:52:37 +0200

    Setup for pcvs-0.8.0 release

--- setup.py
+++ setup.py
@@ -3,16 +3,19 @@ import os
 
 import setuptools
 
-with open(os.path.join("version.txt"), encoding='utf-8') as fh:
+with open(os.path.join("version.txt"), encoding="utf-8") as fh:
     version = fh.readline()[:-1]  # remove \n
 
 try:
     import sh
 
     # pylint fail to parse sh.git function correctly
-    version += "+{}".format(sh.git("rev-parse", "HEAD").strip()[:8])  # pylint: disable=too-many-function-args
-    if sh.git('--no-pager', 'diff', 'HEAD'):  # pylint: disable=too-many-function-args
-        version += ".dirty"
+    if "dev" in version:
+        version += "+{}".format(
+            sh.git("rev-parse", "HEAD").strip()[:8]  # pylint: disable=too-many-function-args
+        )
+        if sh.git("--no-pager", "diff", "HEAD"):  # pylint: disable=too-many-function-args
+            version += ".dirty"
 except Exception:
     pass
 
