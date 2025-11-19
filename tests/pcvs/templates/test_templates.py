07afa5a7368ca6ef1c228eba954be93fa278cb47
Nicolas Marie <nicolas.marie@uvsq.fr>
2025-11-19 16:13:27 +0100

    test(templates): add back templates tests

--- tests/pcvs/templates/test_templates.py
+++ tests/pcvs/templates/test_templates.py
@@ -1,33 +1,10 @@
-# import os
-# from pathlib import Path
-#
-# import pytest
-#
-# from pcvs.backend.configfile import ConfigFile
-# from pcvs.backend.profile import Profile
-# from pcvs.helpers.storage import ConfigDesc
-# from pcvs.helpers.storage import ConfigKind
-# from pcvs.helpers.storage import ConfigScope
+from pcvs.backend.configfile import get_conf
+from pcvs.helpers.storage import ConfigLocator
+from pcvs.helpers.storage import ConfigScope
 
-# TODO re enable template tests
-# def find_files(t):
-#     array = []
-#     d = os.path.join(os.path.dirname(__file__), "..", "..", "..", "pcvs", "templates", t)
-#     for f in os.listdir(d):
-#         if f == "group-compat.yml":
-#             continue
-#         array.append(os.path.join(d, f))
-#     return array
-#
-#
-# @pytest.mark.parametrize("configpath", [*find_files("config")])
-# def test_configuration_templates(configpath):
-#     config = os.path.basename(configpath)
-#     conf_kind = ConfigKind.fromstr(config.split(".")[0])
-#     _ = ConfigFile(ConfigDesc("test", Path(configpath), conf_kind, ConfigScope.LOCAL))
-#
-#
-# @pytest.mark.parametrize("profilepath", [*find_files("profile")])
-# def test_profile_template(profilepath):
-#     _ = Profile(ConfigDesc("tmp", Path(profilepath), ConfigKind.PROFILE, ConfigScope.LOCAL))
-#
+
+def test_configuration_templates():
+    """Test that all configs are valides."""
+    for test_desc in ConfigLocator().list_all_configs(scope=ConfigScope.GLOBAL):
+        cf = get_conf(test_desc)
+        cf.validate()
