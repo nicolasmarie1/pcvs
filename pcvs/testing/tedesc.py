c173d56ff64a50ec2d5044df9b21d1cfd1f9da13
Nicolas Marie <nicolas.marie@uvsq.fr>
2026-01-20 10:27:34 +0100

    fix(group): fix group inheritance
    - fix group inheritance failing to correctly inherit test description
    - fix group global cache being contaminated by invalid update

--- pcvs/testing/tedesc.py
+++ pcvs/testing/tedesc.py
@@ -133,6 +133,16 @@ def build_pm_deps(deps_node: dict[str, Any]) -> list[PManager]:
     return pm.identify(deps_node.get("package_manager", {}))
 
 
+def deepupdate(original: dict, update: dict) -> dict:
+    """Recursively update of a python dictionary."""
+    for key, value in update.items():
+        if isinstance(value, dict):
+            original[key] = deepupdate(original.get(key, {}), value)
+        else:
+            original[key] = value
+    return original
+
+
 class TEDescriptor:
     """A Test Descriptor (named TD, TE or TED), maps a test program
     representation, as defined by a root node in a single test files.
@@ -194,10 +204,10 @@ class TEDescriptor:
         # arregate the 'group' definitions with the TE
         # to get all the fields in their final form
         if "group" in nodecontent and nodecontent["group"] in GlobalConfig.root["group"].keys():
-            tmp = GlobalConfig.root["group"][nodecontent["group"]]
-            tmp.update(nodecontent)
-            nodecontent = tmp
-        # io.console.debug(f"test-descriptor: {nodecontent}")
+            group: dict = GlobalConfig.root["group"][nodecontent["group"]]
+            new_node: dict = copy.deepcopy(group)
+            deepupdate(new_node, nodecontent)
+            nodecontent = new_node
 
         # load from descriptions
         self._build: dict = nodecontent.get("build", {})
@@ -327,7 +337,7 @@ class TEDescriptor:
             test_crits = {}
             # browse declared criterions (system-wide)
             for k_sys, v_sys in self._sys_crit.items():
-                # if we do note inherite from this criterion, skip it
+                # if we do not inherite from this criterion, skip it
                 if (
                     "inherite" in self._run["iterate"]
                     and k_sys not in self._run["iterate"]["inherite"]
